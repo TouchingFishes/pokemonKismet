1e601002cf5e6a149fbf810a27a93f5df64bc552
TouchingFishes <lutzonino03@gmail.com>
2026-09-17 08:01:20 +0200

    Initiated work on the DaoDaoIsles

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -2419,3 +2419,13 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/Akala_Cave_hns/scripts.inc"
 
 .endif
+
+	.include "data/maps/DaoDaoIsles_Isle1_hns/scripts.inc"
+
+	.include "data/maps/DaoDaoIsles_Isle2_hns/scripts.inc"
+
+	.include "data/maps/DaoDaoIsles_Isle3hns/scripts.inc"
+
+	.include "data/maps/DaoDaoIsles_Water_hns/scripts.inc"
+
+	.include "data/maps/DaoDaoIsles_Isle4_hns/scripts.inc"
